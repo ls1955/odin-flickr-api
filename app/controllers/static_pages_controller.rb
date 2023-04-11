6157a55ec2c414187c13cc652e1a8f34c6a34d5e
Cheong <xxlsxx1955@gmail.com>
2023-04-11 21:15:07 +0800

    Include Flickr gem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
+    if params[:flickr_user_id]
+      @response = Flickr.new.people.getPhotos user_id: params[:flickr_user_id]
+    end
   end
 end
